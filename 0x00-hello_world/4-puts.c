3c91a2febdf9a09bd86e06f1583377b949470f61
Janis_Theuri <janistheuri22@gmail.com>
2022-09-08 11:39:33 -0700

    Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>                                                                                                              
 /* This is
  * my
- * program */
-	  int main(void){
-  	puts ("\"Programming is like building a multilingual puzzle\n");                                                         
-
-			                return 0;                               
+ * program 
+ */
+int main (void) {
+puts ("\"Programming is like building a multilingual puzzle\n");
+return 0;                               
 }		
