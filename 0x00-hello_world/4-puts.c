16c89569a02e9326eb090a5632b1957a3550c5c7
Janis_Theuri <janistheuri22@gmail.com>
2022-09-08 12:14:58 -0700

    Coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <stdio.h>                                                                                                              
-/**main = entry point
- * Description = learning c
- * return : always 0 (success)
+#include <stdio.h>
+/**main - entry point
+ *
+ * Return : always 0 (success)
  */
-int main (void) {
-puts("\"Programming is like building a multilingual puzzle");
-return 0;                               
-}		
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return 0;
+}
