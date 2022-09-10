ddb1d2e3a1573cb77fff3c4fee82e52ab7d54d1b
Janis_Theuri <janistheuri22@gmail.com>
2022-09-10 11:44:23 -0700

    Combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*
+*Description: Combination of two numbers
+*
+*Return: Always 0 (sucess)
+*/
+int main(void)
+{
+int n, p;
+	for (n = '0'; n <= '8'; n++)
+{
+	for (p = '1'; p <= '9'; p++)
+{
+		if (n != p)
+	{
+		putchar(n);
+		putchar(p);
+		if (n == '8' && p == '9')
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+}
+}
+	putchar('\n');
+return (0);
+}
