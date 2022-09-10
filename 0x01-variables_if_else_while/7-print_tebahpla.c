d625042cae752618891e796bead9ddf330ab1e56
Janis_Theuri <janistheuri22@gmail.com>
2022-09-10 01:54:15 -0700

    Putchar

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,15 +4,13 @@
 /**
 *main - Entry point
 *
-*Return: Always 0 (success)
+*Return: Always 0 (sucess)
 */
 int main(void)
 {
-int n;
-
-	for (n = 0; n <= 10; n++)
-		putchar(n + '0');
+char low;
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low - 'low');
 	putchar('\n');
-
 return (0);
 }
