84dc88dbfe748fd73bbf378b8b96f1bfa7745ef9
Janis_Theuri <janistheuri22@gmail.com>
2022-09-10 00:58:03 -0700

    Putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+	for (n = 0; n <= 10; n++)
+		putchar(n);
+	putchar('\n');
+return (0);
+}
