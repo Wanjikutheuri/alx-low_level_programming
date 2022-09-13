7c9ac3b83708d1403b02b3f20fa29468ed50bde9
Janis_Theuri <janistheuri22@gmail.com>
2022-09-13 09:49:45 -0700

    Asciing

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_islower - Prints lowercase numbers
+*
+*@c: letter being tested
+*
+*Return: 1 if lower case
+*/
+int _islower(int c)
+{
+
+	if (c >= 0 && c <= 1000)
+		return (1);
+	else
+		return (0);
+}
